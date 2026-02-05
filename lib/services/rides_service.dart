57ac80fc0e7e051ca57b403082c268a6171edafa
SeaHuyty <seahuyty4444@gmail.com>
2026-02-05 11:12:43 +0700

    Upload file

---
+++ lib/services/rides_service.dart
@@ -0,0 +1,37 @@
+import 'package:blabla/model/ride/locations.dart';
+
+import '../data/dummy_data.dart';
+import '../model/ride/ride.dart';
+
+////
+///   This service handles:
+///   - The list of available rides
+///
+class RidesService {
+  static List<Ride> allRides = fakeRides;
+
+  // TODO Create a static method to filter rides by departure location
+  static List<Ride> filterByDeparture(Location departure) {
+    return allRides.where((ride) => ride.departureLocation==(departure)).toList();
+  }
+
+  // TODO Create a static method to filter rides by number of requested seat
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    return allRides.where((ride) => ride.remainingSeats >= seatRequested).toList();
+  }
+
+  // TODO Create a static method to filter : optional departure location, optional requested seat
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+    return allRides.where((ride) {
+      if (departure != null && ride.departureLocation != departure) {
+        return false;
+      }
+
+      if (seatRequested != null && ride.remainingSeats < seatRequested) {
+        return false;
+      }
+
+      return true;
+    }).toList();
+  }
+}
